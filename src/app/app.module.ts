4c78921becb5912c8ec7d26f40b2c3b6e003c630
Zhou Yuwei <zhou.yuwei2008@gmail.com>
2022-06-06 12:04:26 +0200

    inachevé

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EnTeteComponent } from './composants/en-tete/en-tete.component';
+import { AffichageComponent } from './composants/affichage/affichage.component';
+import { PersonnesComponent } from './composants/personnes/personnes.component';
+import { BoutonsComponent } from './composants/boutons/boutons.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EnTeteComponent,
+    AffichageComponent,
+    PersonnesComponent,
+    BoutonsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
